53a4e502e07e508cd23b20d3cf768627458e1e8a
Irina1972-active <ussirina300@gmail.com>
2026-03-20 20:11:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
-// import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
   base: '/CourseWithFigma/',
   plugins: [
     react(),
-   tailwindcss(),  
   ],
   resolve: {
     alias: {
